dfb7fa9cbb29cc46ace5d776bee02247848b5ff0
Scriptman 777 <dave56938@gmail.com>
2022-11-05 12:46:52 +0100

    INIT

---
+++ src/test/java/com/UHK/garbagecollectors/GarbageCollectorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UHK.garbagecollectors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarbageCollectorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
