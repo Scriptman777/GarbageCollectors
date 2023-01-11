5dc8727eea1db4774922c4d2aff8c207402c0a1e
Ivo Bauer <baueriv1@uhk.cz>
2023-01-11 10:53:57 +0100

    !!! Skládky - nefunkční !!!

--- src/main/java/com/UHK/garbagecollectors/control/LandfillController.java
+++ src/main/java/com/UHK/garbagecollectors/control/LandfillController.java
@@ -4,6 +4,11 @@ import com.UHK.garbagecollectors.model.Landfill;
 import com.UHK.garbagecollectors.svc.LandfillService;
 import com.UHK.garbagecollectors.svc.LocationService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.lang.reflect.Array;
+import java.util.List;
 
 @Controller
 public class LandfillController {
@@ -14,6 +19,13 @@ public class LandfillController {
     public LandfillController(LocationService lcs, LandfillService lfs) {
         this.locationService = lcs;
         this.landfillService = lfs;
+    }
+
+    @GetMapping("/skladky")
+    public String skladky(Model model) {
+        model.addAttribute("landfills", landfillService.getLandfills());
+        model.addAttribute("testInt", 54664656);
+        return "skladky";
     }
 
 
