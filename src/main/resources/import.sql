ea820338668c83e25e6da6ced3fd49948283c83f
Scriptman 777 <dave56938@gmail.com>
2023-01-15 13:25:15 +0100

    Import file

---
+++ src/main/resources/import.sql
@@ -0,0 +1,194 @@
+
+
+--======================================================
+
+--            AUTOMATED IMPORT SCRIPT
+
+
+--              DO NOT RUN MANUALLY
+
+--======================================================
+
+
+
+
+-- LOCATIONS
+
+
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (1, 15.832735591458572, 50.20308263245361, 'Hradec Králové', '1219/11a', 'Hradecká');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (3, 15.832521014737381, 50.2038105488883, 'Hradec Králové', '1172/3', 'Hradecká');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (5, 15.832231336163773, 50.204291240946844, 'Hradec Králové', '868/1', 'Hradecká');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (7, 15.831530385541214, 50.20528007915286, 'Hradec Králové', '1250/2', 'Hradecká');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (9, 15.831589394139542, 50.20739845753971, 'Hradec Králové', '227/2', 'Ignáta Ignáta Herrmanna');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (11, 15.828398638295425, 50.20806861177453, 'Hradec Králové', '364/3', 'Československé armády');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (13, 15.828861766385359, 50.20885136726248, 'Hradec Králové', '394/9', 'Československé armády');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (15, 15.831832121282957, 50.207920460165425, 'Hradec Králové', '273/1', 'Komenského');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (17, 15.833100806147002, 50.20533944248342, 'Hradec Králové', '784/4z', 'Brněnská');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (19, 15.82983447161331, 50.21055721020168, 'Hradec Králové', '295/23', 'Československé armády');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (21, 15.90680528727188, 50.269444425333305, 'Černilov', '371', 'Černilov');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (23, 15.912334835568288, 50.26437342128736, 'Černilov', '206', 'Černilov');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (25, 15.915982639828542, 50.26295371822387, 'Černilov', '151', 'Černilov');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (27, 15.923857605496266, 50.2622952908452, 'Černilov', '314', 'Černilov');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (29, 15.9210359216126, 50.26191120400447, 'Černilov', '482', 'Černilov');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (31, 15.929361498394826, 50.2625559194376, 'Černilov', '26', 'Černilov');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (33, 15.934060728588918, 50.26264508152357, 'Černilov', '117', 'Černilov');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (35, 15.865150039002998, 50.30006839390426, 'Smiřice', '95', 'Palackého');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (37, 15.86359435777436, 50.300040980995, 'Smiřice', '90', 'Palackého');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (39, 15.862693135545356, 50.299794264101074, 'Smiřice', '616', 'Palackého');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (41, 15.866507236764534, 50.299794264100925, 'Smiřice', '122', 'Palackého');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (43, 15.868765656755073, 50.29997244865311, 'Smiřice', '105', 'Palackého');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (45, 15.866244380281074, 50.299424186360135, 'Smiřice', '75', 'Komenského');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (47, 15.867741052911384, 50.29941733304157, 'Smiřice', '19', 'Palackého');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (49, 15.86685364504433, 50.299182395144705, 'Smiřice', '11', 'Komenského');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (51, 15.87382966737519, 50.299855943445024, 'Smiřice', '128', 'Palackého');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (53, 15.862489287660225, 50.29854695354826, 'Smiřice', '211', 'Nývltova');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (55, 15.861212556169136, 50.29718995217526, 'Smiřice', '51', 'Nývltova');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (57, 15.860815589234932, 50.296316105045136, 'Smiřice', '307', 'Nývltova');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (59, 15.860472266481025, 50.29590830422446, 'Smiřice', '533', 'Nývltova');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (61, 15.63308703103553, 50.23727248681332, 'Nechanice', '183', 'Žižkova');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (63, 15.63433694043647, 50.23696368373519, 'Nechanice', '100', 'Žižkova');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (65, 15.631021730094062, 50.23547453858711, 'Nechanice', '326', 'Račany');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (67, 15.631611816077339, 50.23584511361625, 'Nechanice', '148', 'Račany');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (69, 15.633682481436836, 50.23679212560432, 'Nechanice', '124', 'Žižkova');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (71, 15.630764238028632, 50.234602989666286, 'Nechanice', '4', 'Račany');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (73, 15.633736125617133, 50.23955070548861, 'Nechanice', '37', 'Husovo nám.');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (75, 15.634690992026435, 50.23937229470224, 'Nechanice', '177', 'Husovo nám.');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (77, 15.80334930949141, 50.18755339374211, 'Hradec Králové', '555', 'Pražské Předměstí');
+INSERT INTO garbageCollectors.location (id, gpslat, gpslon, city, house_number, street) VALUES (79, 15.963735566434707, 50.240274936296565, 'Librantice', '123', 'Librantice');
+
+
+
+-- GCANS
+
+
+
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (2, 'Bio', 120, 1);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (4, 'Směsný', 240, 3);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (6, 'Směsný', 120, 5);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (8, 'Papír', 120, 7);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (10, 'Směsný', 120, 9);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (12, 'Směsný', 120, 11);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (14, 'Plasty', 120, 13);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (16, 'Směsný', 120, 15);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (18, 'Bio', 240, 17);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (20, 'Bio', 240, 19);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (22, 'Směsný', 120, 21);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (24, 'Bio', 120, 23);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (26, 'Směsný', 120, 25);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (28, 'Papír', 240, 27);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (30, 'Směsný', 120, 29);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (32, 'Bio', 120, 31);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (34, 'Směsný', 240, 33);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (36, 'Směsný', 240, 35);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (38, 'Směsný', 120, 37);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (40, 'Bio', 120, 39);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (42, 'Směsný', 240, 41);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (44, 'Papír', 120, 43);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (46, 'Směsný', 120, 45);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (48, 'Směsný', 120, 47);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (50, 'Směsný', 240, 49);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (52, 'Bio', 120, 51);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (54, 'Směsný', 120, 53);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (56, 'Plasty', 120, 55);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (58, 'Směsný', 240, 57);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (60, 'Bio', 120, 59);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (62, 'Směsný', 120, 61);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (64, 'Bio', 240, 63);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (66, 'Směsný', 120, 65);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (68, 'Směsný', 240, 67);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (70, 'Papír', 240, 69);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (72, 'Směsný', 120, 71);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (74, 'Papír', 240, 73);
+INSERT INTO garbageCollectors.gcan (id, garbage_type, volume, location_id) VALUES (76, 'Plasty', 240, 75);
+
+
+
+-- LANDFILLS
+
+
+INSERT INTO garbageCollectors.landfill (id, capacity, location_id) VALUES (78, 144000000, 77);
+INSERT INTO garbageCollectors.landfill (id, capacity, location_id) VALUES (80, 300000000, 79);
+
+
+
+-- TRUCKS
+
+
+INSERT INTO garbageCollectors.gtruck (id, licence_plate, make, model, capacity, home_landfill_id) VALUES (1, '3H5 2568', 'SCANIA', 'P250', 16000, 78);
+INSERT INTO garbageCollectors.gtruck (id, licence_plate, make, model, capacity, home_landfill_id) VALUES (2, '1H3 ODPAD', 'SCANIA', 'P320', 20000, 80);
+INSERT INTO garbageCollectors.gtruck (id, licence_plate, make, model, capacity, home_landfill_id) VALUES (3, '8H1 1324', 'DAF', 'CF FAN', 18000, 80);
+INSERT INTO garbageCollectors.gtruck (id, licence_plate, make, model, capacity, home_landfill_id) VALUES (4, '5H5 5555', 'MAN', 'TGS 28', 19000, 78);
+
+
+
+-- SUPPORTED TYPES
+
+
+INSERT INTO garbageCollectors.gtruck_supported_garbage_types (gtruck_id, supported_garbage_types) VALUES (1, 'Směsný');
+INSERT INTO garbageCollectors.gtruck_supported_garbage_types (gtruck_id, supported_garbage_types) VALUES (1, 'Plasty');
+INSERT INTO garbageCollectors.gtruck_supported_garbage_types (gtruck_id, supported_garbage_types) VALUES (1, 'Papír');
+INSERT INTO garbageCollectors.gtruck_supported_garbage_types (gtruck_id, supported_garbage_types) VALUES (1, 'Sklo');
+INSERT INTO garbageCollectors.gtruck_supported_garbage_types (gtruck_id, supported_garbage_types) VALUES (1, 'Kovy');
+INSERT INTO garbageCollectors.gtruck_supported_garbage_types (gtruck_id, supported_garbage_types) VALUES (1, 'Recyklovatelný');
+INSERT INTO garbageCollectors.gtruck_supported_garbage_types (gtruck_id, supported_garbage_types) VALUES (1, 'Bio');
+INSERT INTO garbageCollectors.gtruck_supported_garbage_types (gtruck_id, supported_garbage_types) VALUES (2, 'Směsný');
+INSERT INTO garbageCollectors.gtruck_supported_garbage_types (gtruck_id, supported_garbage_types) VALUES (2, 'Recyklovatelný');
+INSERT INTO garbageCollectors.gtruck_supported_garbage_types (gtruck_id, supported_garbage_types) VALUES (2, 'Bio');
+INSERT INTO garbageCollectors.gtruck_supported_garbage_types (gtruck_id, supported_garbage_types) VALUES (3, 'Směsný');
+INSERT INTO garbageCollectors.gtruck_supported_garbage_types (gtruck_id, supported_garbage_types) VALUES (3, 'Plasty');
+INSERT INTO garbageCollectors.gtruck_supported_garbage_types (gtruck_id, supported_garbage_types) VALUES (3, 'Papír');
+INSERT INTO garbageCollectors.gtruck_supported_garbage_types (gtruck_id, supported_garbage_types) VALUES (3, 'Sklo');
+INSERT INTO garbageCollectors.gtruck_supported_garbage_types (gtruck_id, supported_garbage_types) VALUES (3, 'Kovy');
+INSERT INTO garbageCollectors.gtruck_supported_garbage_types (gtruck_id, supported_garbage_types) VALUES (3, 'Recyklovatelný');
+INSERT INTO garbageCollectors.gtruck_supported_garbage_types (gtruck_id, supported_garbage_types) VALUES (3, 'Bio');
+INSERT INTO garbageCollectors.gtruck_supported_garbage_types (gtruck_id, supported_garbage_types) VALUES (4, 'Nebezpečný');
+INSERT INTO garbageCollectors.gtruck_supported_garbage_types (gtruck_id, supported_garbage_types) VALUES (4, 'Bio');
+
+
+
+-- COLLECTIONS
+
+
+INSERT INTO garbageCollectors.gcollection (id, name, assigned_truck_id) VALUES (81, 'Hradec - Vše', 1);
+INSERT INTO garbageCollectors.gcollection (id, name, assigned_truck_id) VALUES (82, 'Smiřice - směsný', 2);
+INSERT INTO garbageCollectors.gcollection (id, name, assigned_truck_id) VALUES (83, 'Nechanice - vše', 3);
+INSERT INTO garbageCollectors.gcollection (id, name, assigned_truck_id) VALUES (84, 'Černilov - Směsný', 1);
+INSERT INTO garbageCollectors.gcollection (id, name, assigned_truck_id) VALUES (85, 'Černilov - Bio', 4);
+
+
+
+-- CANS IN COLLECTIONS
+
+
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (81, 4);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (81, 16);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (81, 10);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (81, 20);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (81, 18);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (81, 2);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (81, 12);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (81, 8);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (81, 14);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (81, 6);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (82, 38);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (82, 50);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (82, 42);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (82, 48);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (82, 54);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (82, 58);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (82, 36);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (82, 46);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (83, 72);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (83, 66);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (83, 68);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (83, 62);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (83, 76);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (83, 64);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (83, 74);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (83, 70);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (84, 22);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (84, 34);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (84, 30);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (84, 26);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (85, 32);
+INSERT INTO garbageCollectors.gcollection_cans (gcollection_id, cans_id) VALUES (85, 24);
